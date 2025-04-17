7856b365a3be299be26625c95a3b0341c3124b7b
Sachintha Hasaranga <sachinthahasaranga0415@gmail.com>
2025-04-17 10:00:00 +0530

    feedback middleware  modification

--- feedback/middleware/auth.js
+++ feedback/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token> will be getting from here 
 
   if (!token) return res.status(401).json({ message: 'Token missing' });
 
